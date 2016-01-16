3ad27aca797034999649db7ff979e28e68e1c5b6
Ben Townsend <btownsend220@gmail.com>
2016-01-16 16:12:08 +0000

    first commit

---
+++ new_machine.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ~
+git clone https://github.com/ben198/dotfiles.git
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s ~/dotfiles/vimrc ~/.vimrc
+vim +PluginInstall +qall
