62df7663f8e24bf1118c7870bf79bf6165adbcf8
ben_townsend <ben.townsend@learnsci.co.uk>
2016-03-26 10:07:59 +0000

    symlinks are created for bashrc and inputrc

--- new_machine.sh
+++ new_machine.sh
@@ -3,5 +3,7 @@
 cd ~
 git clone https://github.com/ben198/dotfiles.git
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/inputrc ~/.inputrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
 vim +PluginInstall +qall
