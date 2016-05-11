b22cdc2a2a131d4a88d438954776add07d9acc91
Ben Townsend <btownsend220@gmail.com>
2016-05-11 22:05:07 +0100

    colours changed in terminal

--- new_machine.sh
+++ new_machine.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 cd ~
+git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
+cd gnome-terminal-colors-solarized/
+sudo apt-get install dconf-cli
+./install.sh
+cd ~
 rm -rf .vim
 git clone https://github.com/ben198/dotfiles.git
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
